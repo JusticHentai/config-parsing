99a562674315802978b0c70fa1fc786052bc9a8f
JusticHentai <1360636180@qq.com>
2024-08-13 18:08:10 +0800

    版本更新

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,36 @@
+import { getBabelOutputPlugin } from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
-import { terser } from 'rollup-plugin-terser'
-import typescript from 'rollup-plugin-typescript2'
 
 export default [
   {
     input: './index.ts',
     output: [
       {
-        file: './dist/my-component.js',
+        file: './dist/utils.js',
         format: 'es',
-      },
-      {
-        file: './dist/my-component.cjs',
-        format: 'cjs',
       },
     ],
     plugins: [
-      // 使得 rollup 支持 commonjs 规范，识别 commonjs 规范的依赖
-      commonjs(),
-      // 配合 commonjs 解析第三方模块
       resolve(),
-      // ts
+      commonjs(),
       typescript(),
-      // 打包后压缩回车和空格
+      getBabelOutputPlugin({
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              modules: false,
+              useBuiltIns: 'usage',
+              corejs: '3.38.0',
+            },
+          ],
+        ],
+        plugins: ['@babel/plugin-transform-runtime'],
+      }),
       terser(),
     ],
   },
@@ -32,7 +38,7 @@ export default [
     input: './index.ts',
     output: [
       {
-        file: './dist/my-component.d.ts',
+        file: './dist/utils.d.ts',
         format: 'es',
       },
     ],
